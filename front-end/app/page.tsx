d893db0029ac14a7dde8af1cd5b68836472a4021
asipita <sa.abdulgafar@gmail.com>
2023-09-25 02:12:23 +0100

    track subdirs

---
+++ front-end/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import GridLines from "@/public/assets/gradient-lines.png";
+export default function Home() {
+  return (
+    <main className="flex h-screen flex-col items-center justify-center p-24 bg-gradient-to-b from-[#7C69D8] to-[#D7603C] relative">
+      <div className="absolute w-full h-full left-0 top-0">
+        <Image src={GridLines} alt="" className="w-screen h-screen" />
+      </div>
+
+      <div className="flex flex-col gap-10">
+        <h1 className="text-7xl text-white font-bold">Voauth</h1>
+        <h4 className="text-3xl font-light text-white animate-pulse text-center">
+          Coming Soon...
+        </h4>
+      </div>
+    </main>
+  );
+}
